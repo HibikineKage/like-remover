551dfe0c43519bbc9f94dcb2ed41bdba06ff5eef
Hibikine Kage <goodsmile314159@gmail.com>
2018-02-04 22:04:27 +0900

    first commit

---
+++ myscript.js
@@ -0,0 +1,29 @@
+// 他人のいいねを消す処理
+doJudge = (i, elem) => {
+    const checked = "likeremover-checked";
+    
+    // チェック済みかどうか判定
+    if($(elem).hasClass(checked)) {
+        return;
+    }
+    
+    const json_raw = $(elem).attr('data-suggestion-json');
+    const json = JSON.parse(json_raw);
+    if(json["suggestion_details"]["suggestion_type"] === "ActivityTweet") {
+        $(elem).addClass("likermover-like-tweet")
+    }
+    
+    
+    $(elem).addClass(checked);
+}
+
+// DOM変更時に行う処理
+runTask = () => {
+    $('li[data-suggestion-json]').each(doJudge);
+}
+
+// DOM変更を検出
+let target = document.getElementsByTagName('html')[0];
+let observer = new MutationObserver(runTask);
+let config = {childList: true, subtree: true};
+observer.observe(target, config)
